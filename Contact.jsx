e5fd7b23b60f65c53aaf343cb45fce873e0d1fe5
Prabhat Solanki <155136172+prabhats-github@users.noreply.github.com>
2024-02-04 21:27:22 +0530

    Add files via upload
    Design Pearl project for practice with react js.....

--- Contact.jsx
+++ Contact.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import style from './Contact.module.css'
+
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className={`${style.d1}`}>
+      <input id={`${style.inp1}`} type="text" placeholder='Email' />
+      <input type="text" placeholder='Password' />
+      <div><p>Show Password <i id={`${style.toggleon}`} class="fa-solid fa-toggle-on"></i></p>
+      <p  id={style.forgot}> <i class="fa-solid fa-lock"></i>Forgot Password?</p>
+      
+      </div>
+      <div id={`${style.login}`}>
+        <p>LOG IN</p>
+      </div>
+
+      <p id={`${style.privacy}`}>
+        Privacy | Cookies | Terms of Use
+      </p>
+      
+    </div>
   )
 }
 
--- Nav.jsx
+++ Nav.jsx
@@ -4,10 +4,20 @@ import style from './Nav.module.css'
 const Nav = () => {
   return (
     <div className={`${style.navdiv}`}>
+     
         <div id={`${style.client}`}>
-            <h4>PEARL <br /> CLIENT WORKSPACE</h4>
-            <div id=''></div>
+            <h4 id={`${style.h4}`}>PEARL <br /> CLIENT WORKSPACE</h4>
+            
         </div>
+        <div id={`${style.logos}`}>
+        <i class="fa-brands fa-facebook"></i>
+        <i class="fa-brands fa-square-twitter"></i>
+        <i class="fa-brands fa-linkedin"></i>
+        <i class="fa-brands fa-pinterest"></i>
+        <i class="fa-brands fa-square-youtube"></i>
+        </div>
+        <p id={`${style.p1}`}>© 2024 Pearl Organisation All rights reserved.</p>
+        <p id={`${style.trademark}`}>PEARL ORGANISATION <sup>TM</sup> is a registered trademark of VUNUM Infotech Solutions PVT. LTD. company.</p>
     </div>
   )
 }
