dca03d19bc580556dcf534591ceef47205122718
Prabhat Solanki <155136172+prabhats-github@users.noreply.github.com>
2024-02-04 21:41:39 +0530

    Add files via upload
    Design Pearl Project using react js (For Practicing...)

--- Nav.jsx
+++ Nav.jsx
@@ -10,11 +10,12 @@ const Nav = () => {
             
         </div>
         <div id={`${style.logos}`}>
-        <i class="fa-brands fa-facebook"></i>
-        <i class="fa-brands fa-square-twitter"></i>
-        <i class="fa-brands fa-linkedin"></i>
-        <i class="fa-brands fa-pinterest"></i>
-        <i class="fa-brands fa-square-youtube"></i>
+        <a href="https://www.facebook.com/login/"><i class="fa-brands fa-facebook"></i></a>
+
+        <a href="https://twitter.com/i/flow/login"><i  class="fa-brands fa-square-twitter"></i></a>
+       <a href="https://www.linkedin.com/login"><i class="fa-brands fa-linkedin"></i></a> 
+        <a href="https://in.pinterest.com/login/"><i class="fa-brands fa-pinterest"></i></a>
+        <a href="https://www.youtube.com/"><i class="fa-brands fa-square-youtube"></i></a>
         </div>
         <p id={`${style.p1}`}>© 2024 Pearl Organisation All rights reserved.</p>
         <p id={`${style.trademark}`}>PEARL ORGANISATION <sup>TM</sup> is a registered trademark of VUNUM Infotech Solutions PVT. LTD. company.</p>
